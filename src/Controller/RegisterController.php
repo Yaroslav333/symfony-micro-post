bc3314b90f3c100972fde9f4493bee50413f2486
Yaroslav333 <yarik1848@gmail.com>
2019-04-19 17:21:11 +0300

    added registration form

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function register(UserPasswordEncoderInterface $passwordEncoder, Request $request, RouterInterface $router)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword(
+                $user,
+                $user->getPlainPassword()
+            );
+            $user->setPassword($password);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return new RedirectResponse($router->generate('micro_post_index'));
+        }
+
+        return $this->render('register/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
